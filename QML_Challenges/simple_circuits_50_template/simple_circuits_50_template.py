92365dd20680549e09a35f8aa54db04c51394787
jchen8tw <11806483+jchen8tw@users.noreply.github.com>
2021-02-21 16:12:57 +0800

    simple_circuits_50_template.py

--- QML_Challenges/simple_circuits_50_template/simple_circuits_50_template.py
+++ QML_Challenges/simple_circuits_50_template/simple_circuits_50_template.py
@@ -25,12 +25,19 @@ def simple_circuits_50(angle):
     # QHACK #
 
     # Step 1 : initialize a device
-
+    dev = qml.device('default.qubit', wires=2)
     # Step 2 : Create a quantum circuit and qnode
 
+    @qml.qnode(dev)
+    def my_quantum_func(theta):
+        qml.Hadamard(wires=0)
+        qml.CNOT(wires=[0,1])
+        qml.RY(angle,wires=0)
+        return qml.expval(qml.PauliZ(0) @ qml.PauliZ(1))
     # Step 3 : Run the qnode
     # expectation_value = ?
-
+    expectation_value = my_quantum_func(angle)
+    # print(my_quantum_func.draw())
     # QHACK #
     return expectation_value
 
